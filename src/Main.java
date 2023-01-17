958b1d3cee1295fff807f5d402bd966b1ad1e22a
monikah0204 <moniaharazin@gmail.com>
2023-01-17 22:45:59 +0100

    Added mixed quiz

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Quiz geographyQuiz = new Quiz(scanner,new GeographyQuizBody().getQuiz());
-        Quiz biologyQuiz = new Quiz(scanner,new BiologyQuizBody().getQuiz());
+        List<QuizPart> biologyQuizParts = new BiologyQuizBody().getQuiz();
+        List<QuizPart> geographyQuizParts = new GeographyQuizBody().getQuiz();
+        List<QuizPart> mixedQuizParts = new ArrayList<>();
+        mixedQuizParts.addAll(biologyQuizParts);
+        mixedQuizParts.addAll(geographyQuizParts);
+
+        Quiz geographyQuiz = new Quiz(scanner, geographyQuizParts);
+        Quiz biologyQuiz = new Quiz(scanner, biologyQuizParts);
+        Quiz mixedQuiz = new Quiz(scanner, mixedQuizParts);
 
         try {
             System.out.println("Welcome");
-            System.out.println("1.Biology Quiz \n2.Geography Quiz");
+            System.out.println("1.Biology Quiz \n2.Geography Quiz \n3.Mixed Quiz");
             System.out.print("Choose quiz: ");
             int userQuiz = scanner.nextInt();
             System.out.println();
             if (userQuiz == 1) {
                 biologyQuiz.runQuiz();
-            } else {
+            } else if (userQuiz == 2) {
                 geographyQuiz.runQuiz();
+            } else {
+                mixedQuiz.runQuiz();
             }
 
         } catch (InterruptedException e) {
